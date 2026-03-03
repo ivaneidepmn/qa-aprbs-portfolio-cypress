015f2dd2a9de36ccd653b629e4741f28139f5c02
Ivaneide Pires Monteiro do Nascimento <ivapmn@gmail.com>
2026-03-03 16:14:52 -0300

    update QA portfolio and Cypress smoke tests

--- cypress/e2e/smoke-tests.cy.js
+++ cypress/e2e/smoke-tests.cy.js
@@ -1,1 +1,18 @@
--
+-describe('APRBS Certification Website', () => {
+
+  it('Deve carregar a página inicial', () => {
+    cy.visit('https://qualidade.apprbs.com.br/certificacao')
+    cy.contains('Certificação').should('be.visible')
+  })
+
+  it('Deve verificar se o botão de acesso existe', () => {
+    cy.visit('https://qualidade.apprbs.com.br/certificacao')
+    cy.get('button').should('exist')
+  })
+
+  it('Deve validar se a página possui formulário', () => {
+    cy.visit('https://qualidade.apprbs.com.br/certificacao')
+    cy.get('input').should('exist')
+  })
+
+})
