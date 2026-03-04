58b0baae9990f654a0cec1a059dbf48243691cd7
Ivaneide Pires Monteiro do Nascimento <ivapmn@gmail.com>
2026-03-03 23:45:52 -0300

    chore: improve CI and Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -5,14 +5,23 @@ module.exports = defineConfig({
   pageLoadTimeout: 60000,
   screenshotOnRunFailure: true,
 
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome",
+    overwrite: false,
+    html: false,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true,
+  },
+
   e2e: {
     baseUrl: "https://qualidade.apprbs.com.br",
     specPattern: "cypress/e2e/**/*.cy.js",
     supportFile: false,
-
     viewportWidth: 1366,
     viewportHeight: 768,
-
     video: true,
     screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
