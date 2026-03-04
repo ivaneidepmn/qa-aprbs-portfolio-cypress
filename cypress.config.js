013b7d76e1109c2f1af850d927670d9e0cddd5df
Ivaneide Pires Monteiro do Nascimento <ivapmn@gmail.com>
2026-03-04 00:09:00 -0300

    chore: add Mochawesome report and upload artifacts in CI

--- cypress.config.js
+++ cypress.config.js
@@ -1,21 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  defaultCommandTimeout: 10000,
-  pageLoadTimeout: 60000,
-  screenshotOnRunFailure: true,
-
-  reporter: "mochawesome",
-  reporterOptions: {
-    reportDir: "cypress/reports/mochawesome",
-    overwrite: false,
-    html: false,
-    json: true,
-    charts: true,
-    embeddedScreenshots: true,
-    inlineAssets: true,
-  },
-
   e2e: {
     baseUrl: "https://qualidade.apprbs.com.br",
     specPattern: "cypress/e2e/**/*.cy.js",
@@ -23,11 +8,17 @@ module.exports = defineConfig({
     viewportWidth: 1366,
     viewportHeight: 768,
     video: true,
+
     screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
 
-    setupNodeEvents(on, config) {
-      return config;
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: false,
+      json: true,
+      timestamp: "mmddyyyy_HHMMss",
     },
   },
 });
