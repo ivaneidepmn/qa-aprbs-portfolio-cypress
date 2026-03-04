71589108f38e0334e60549048536b0790044b51a
Ivaneide Pires Monteiro do Nascimento <ivapmn@gmail.com>
2026-03-04 01:01:58 -0300

    ci: add Cypress pipeline with Mochawesome report, videos and screenshots

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,29 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    reportDir: "cypress/reports/mochawesome",
+    overwrite: false,
+    html: false,
+    json: true,
+    charts: true,
+    embeddedScreenshots: true,
+    inlineAssets: true
+  },
+
   e2e: {
     baseUrl: "https://qualidade.apprbs.com.br",
     specPattern: "cypress/e2e/**/*.cy.js",
-    supportFile: false,
+    supportFile: "cypress/support/e2e.js",
     viewportWidth: 1366,
     viewportHeight: 768,
     video: true,
-
     screenshotsFolder: "cypress/screenshots",
     videosFolder: "cypress/videos",
-
-    reporter: "mochawesome",
-    reporterOptions: {
-      reportDir: "cypress/reports/mochawesome",
-      overwrite: false,
-      html: false,
-      json: true,
-      timestamp: "mmddyyyy_HHMMss",
-    },
-  },
+    setupNodeEvents(on, config) {
+      require("cypress-mochawesome-reporter/plugin")(on);
+      return config;
+    }
+  }
 });
